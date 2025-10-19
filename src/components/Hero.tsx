17d1c68a8d760b661790bce4d165dac72f47d3b1
Spandan-Mishra <spandanmishra69@gmail.com>
2025-10-19 19:45:34 +0530

    feat: simple hero animation added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,39 @@
+"use client";
+
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { useEffect, useRef } from "react";
+
+gsap.registerPlugin(SplitText);
 
 const Hero = () => {
+    const heroRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const hero = heroRef.current;
+
+        if (!hero) return;
+
+        SplitText.create(hero, {
+            type: "lines, words",
+            mask: "lines",
+            autoSplit: true,
+            onSplit(self) {
+                return gsap.from(self.words, {
+                    duration: 1,
+                    y: 100,
+                    autoAlpha: 0,
+                    stagger: 0.05,
+                });
+            },
+        });
+    }, []);
+
     return (
-        <div className="text-9xl flex min-h-screen items-center justify-center">
+        <div ref={heroRef} className="text-9xl flex min-h-screen items-center justify-center">
             I am Spandan Mishra
         </div>
-    )
-}
+    );
+};
 
-export default Hero
+export default Hero;
