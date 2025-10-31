a20fe5193b800626acb381986cdc639d4c618b87
Spandan-Mishra <spandanmishra69@gmail.com>
2025-10-31 13:51:01 +0530

    update: hero section bg gridlines

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import gsap from "gsap";
 import { SplitText } from "gsap/SplitText";
 import { useEffect, useRef } from "react";
@@ -30,9 +31,20 @@ const Hero = () => {
     }, []);
 
     return (
-        <div ref={heroRef} className="text-8xl flex min-h-screen items-center justify-center">
-            I am Spandan Mishra
-        </div>
+        <div className="relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:40px_40px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+        )}
+      />
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      <p ref={heroRef} className="relative z-20 bg-gradient-to-b from-primary to-gray-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-8xl">
+        I am Spandan Mishra
+      </p>
+    </div>
     );
 };
 
